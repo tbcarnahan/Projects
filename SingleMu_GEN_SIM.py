ab5c36acad97ff289644753aa3d4c20d1fa9ba3e
tbcarnahan <71470193+tbcarnahan@users.noreply.github.com>
2020-12-07 12:22:35 -0600

    Update SingleMu_GEN_SIM.py

--- SingleMu_GEN_SIM.py
+++ SingleMu_GEN_SIM.py
@@ -100,6 +100,25 @@ process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2021_realistic','')
 
+#gives full range
+generator = cms.EDProducer("FlatRandomOneOverPtGunProducer",
+    PGunParameters = cms.PSet(
+        # This specifies range in 1/Pt                                                       
+        # It coresponds to Pt = 0.1 to 2000 GeV                                              
+        MinOneOverPt = cms.double(0.1),
+        MaxOneOverPt = cms.double(1.),
+        PartID = cms.vint32(-13),
+        MaxEta = cms.double(2.5),
+        MaxPhi = cms.double(3.14159265359),
+        MinEta = cms.double(-2.5),
+        MinPhi = cms.double(-3.14159265359) ## in radians                                    
+    ),
+    Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts              
+    psethack = cms.string('single mu pt 0.1to10'),
+    AddAntiParticle = cms.bool(True),
+    firstRun = cms.untracked.uint32(1)
+)
+
 process.generator = cms.EDFilter("Pythia8PtGun",
     PGunParameters = cms.PSet(
         AddAntiParticle = cms.bool(True),
